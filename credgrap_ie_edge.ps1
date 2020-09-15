14c466cb8d6b0c05f83a7413b2ec409dd3bd31d1
Florian Hansemann <info@hansesecure.de>
2020-09-15 12:52:43 +0200

    Update credgrap_ie_edge.ps1

--- credgrap_ie_edge.ps1
+++ credgrap_ie_edge.ps1
@@ -1,4 +1,6 @@
-
+# Content: Get the stored creds from Internet Explorer and Edge
+# Author: Florian Hansemann | @CyberWarship | https://hansesecure.de
+# Date: 09/2020
 
 [void][Windows.Security.Credentials.PasswordVault,Windows.Security.Credentials,ContentType=WindowsRuntime]
 $vault = New-Object Windows.Security.Credentials.PasswordVault
